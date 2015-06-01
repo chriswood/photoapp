f2d496502911656e53f1fb3722868ffca6e27308
chriswood <chris314159@gmail.com>
2015-05-31 20:10:51 -0500

    Figuring stuff out with pyqt5

---
+++ README.txt
@@ -0,0 +1,18 @@
+
+yee master plan
+
+I want an app to copy my files from my phone, deduping automatically, to
+a folder on my computer. It should also give me the option of naming them
+individually, or en mass
+
+1) get basic gui function with a few buttons
+
+2) be able to push a button and copy file from phone to folder
+
+2.5) look at drag and drop
+
+3) dedupe these files
+
+4) have option to rename them
+
+5) have this open automatic when I plug my phone in
--- main.py
+++ main.py
@@ -3,6 +3,7 @@
 
 import sys
 from PyQt5.QtWidgets import QWidget, QToolTip, QPushButton, QApplication
+from PyQt5.QtWidgets import QHBoxLayout, QVBoxLayout
 from PyQt5.QtGui import QFont
 from PyQt5.QtCore import QCoreApplication
 
@@ -11,31 +12,24 @@ class Main(QWidget):
     def __init__(self, title):
         super().__init__()
         self.setWindowTitle(title)
-        self.initUI()
-
-
-    def initUI(self):
-        # set window tool tip
         QToolTip.setFont(QFont('SansSerif', 10))
-        self.setToolTip('This is a <b>QWidget</b> widget')
-
-        # resize button
-        btn1 = QPushButton('Button', self)
-        btn1.setToolTip('resize')
-        btn1.clicked.connect(lambda: self.resize(btn1))
-        btn1.move(50, 50)
-
-        #exit button
-        qbtn = QPushButton('Quit', self)
-        qbtn.clicked.connect(QCoreApplication.instance().quit)
-
-        self.setGeometry(300, 300, 300, 200)
+        self.setGeometry(400, 400, 300, 200)
+        self.build_buttons()
         self.show()
 
-    def resize(self, btn):
-        btn.resize(200, 100)
+    def build_buttons(self):
+        #exit button
+        #btn1.clicked.connect(lambda: self.resize(btn1))
+        qbtn = QPushButton('Close', self)
+        qbtn.clicked.connect(QCoreApplication.instance().quit)
+
+        hbox = QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(qbtn)
+        self.setLayout(hbox)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    ex = Main('Image Handler Main')
+    ex = Main('RaspConnect')
     sys.exit(app.exec_())
